9aaa1593eb8b3ed5bf8588f283c4c8e94596dfb7
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-02-12 10:34:47 +0100

    add test for Books Generators

---
+++ org.javacream.training.telekom.academy/src/test/java/org/javacream/books/warehouse/BooksJavaTests.java
@@ -0,0 +1,43 @@
+package org.javacream.books.warehouse;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+import org.javacream.books.warehouse.api.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class BooksJavaTests {
+
+	@Autowired
+	@Qualifier("generatorsMap")
+	private Map<Set<String>, Function<Map<String, Object>, Book>> generators;
+
+	@Test public void testCreateSchoolBook() {
+		HashMap<String, Object> optionsForSchoolBook = new HashMap<>();
+		optionsForSchoolBook.put("subject", "physics");
+		optionsForSchoolBook.put("year", 10);
+		
+		
+		Function<Map<String, Object>, Book> generator = generators.get(optionsForSchoolBook.keySet());
+		Book book = generator.apply(optionsForSchoolBook);
+		System.out.println(book.getClass().getName());
+		
+		HashMap<String, Object> optionsForSpecialistBook = new HashMap<>();
+		Function<Map<String, Object>, Book> generator2 = generators.get(optionsForSpecialistBook.keySet());
+		optionsForSpecialistBook.put("topic", "Java");
+		Book book2 = generator2.apply(optionsForSpecialistBook);
+		System.out.println(book2.getClass().getName());
+		
+	}
+}
